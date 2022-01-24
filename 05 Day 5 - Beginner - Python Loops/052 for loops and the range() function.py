70bc7380dedba8d51a5fb5c3cccf5af6323a3397
aatansen <aatansen@gmail.com>
2022-01-24 22:26:49 +0600

    Create 052 for loops and the range() function.py

---
+++ 05 Day 5 - Beginner - Python Loops/052 for loops and the range() function.py
@@ -0,0 +1,13 @@
+# For loop with range:
+# for number in range(1,10):
+#     print(number)
+    
+# Sum of 1 to 100 numbers:
+add=0
+for number in range(1,101):
+    add=number+add 
+print(add)
+    
+
+    
+    
