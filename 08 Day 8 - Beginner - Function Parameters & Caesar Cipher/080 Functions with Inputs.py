952e8c343013f913bda2e12b16f3b872ba5d8211
aatansen <aatansen@gmail.com>
2022-01-28 15:50:31 +0600

    Create 080 Functions with Inputs.py

---
+++ 08 Day 8 - Beginner - Function Parameters & Caesar Cipher/080 Functions with Inputs.py
@@ -0,0 +1,15 @@
+def greet():
+    print("Hello")
+    print("new")
+    print("world")
+
+greet()
+
+print("\n")
+
+def greet_with_name(name):
+    print(f"Hello {name}")
+    print(f"How are you {name}")
+    
+greet_with_name("Tansen")
+    
