f43315466cef8b3806eed15f36b007d910510ce9
aatansen <aatansen@gmail.com>
2022-01-28 16:13:44 +0600

    Create 082 Exercise 1 - Paint Area Calculator.py

---
+++ 08 Day 8 - Beginner - Function Parameters & Caesar Cipher/082 Exercise 1 - Paint Area Calculator.py
@@ -0,0 +1,24 @@
+#Write your code below this line 👇
+import math
+def paint_calc(height,width,cover):
+    cans_number=math.ceil((height*width)/cover)
+    print(f"You'll need {cans_number} cans of paint.")
+
+
+
+
+
+#Write your code above this line 👆
+# Define a function called paint_calc() so that the code below works.   
+
+# 🚨 Don't change the code below 👇
+test_h = int(input("Height of wall: "))
+test_w = int(input("Width of wall: "))
+coverage = 5
+paint_calc(height=test_h, width=test_w, cover=coverage)
+
+# Example Input
+# test_h = 3
+# test_w = 9
+# Example Output
+# You'll need 6 cans of paint.
