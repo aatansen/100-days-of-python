1d63b40e2f5bbe4bead1ff5bbfe3b6b8b0d49bbf
aatansen <aatansen@gmail.com>
2022-01-28 15:58:18 +0600

    Create 081 Positional vs. Keyword Arguments.py

---
+++ 08 Day 8 - Beginner - Function Parameters & Caesar Cipher/081 Positional vs. Keyword Arguments.py
@@ -0,0 +1,11 @@
+# Positional argument:
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+greet_with("Tansen","Bangladesh")
+
+# Keyword argument:
+def greet_with(name,location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+greet_with(name="Tansen",location="Bangladesh")
