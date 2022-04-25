eb66b33c6397666e43755cfd83da4ce3d2b9ad29
Tansen <aatansen@gmail.com>
2022-04-25 19:55:07 +0600

    Create 06 randomly choice from a list.py

---
+++ _Notes/06 randomly choice from a list.py
@@ -0,0 +1,8 @@
+# from day 11: 109
+import random
+def deal_card():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
+x=deal_card()
+print(x)
