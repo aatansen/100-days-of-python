36ed66851356a1cc67d6c53148f4df4335970e66
Tansen <aatansen@gmail.com>
2022-05-04 04:14:37 +0600

    Create 124 Play Computer and Evaluate Each Line.py

---
+++ 13 Day 13 - Beginner - Debugging_ How to Find and Fix Errors in your Code/124 Play Computer and Evaluate Each Line.py
@@ -0,0 +1,8 @@
+############DEBUGGING#####################
+
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year >= 1994: # As 1994 not giving output so = added here
+  print("You are a Gen Z.")
