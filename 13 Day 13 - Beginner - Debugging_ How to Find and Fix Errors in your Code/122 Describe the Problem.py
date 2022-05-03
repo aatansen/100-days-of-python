86bb1b513bf9e28042b19a5f8224289d71b08243
Tansen <aatansen@gmail.com>
2022-05-04 04:04:59 +0600

    Update 122 Describe the Problem.py

--- 13 Day 13 - Beginner - Debugging_ How to Find and Fix Errors in your Code/122 Describe the Problem.py
+++ 13 Day 13 - Beginner - Debugging_ How to Find and Fix Errors in your Code/122 Describe the Problem.py
@@ -2,7 +2,7 @@
 
 # Describe Problem
 def my_function():
-  for i in range(1, 21):
+  for i in range(1, 21): # range end does not count so it will end at 21-1
     # print(i)
     if i == 20:
       print("You got it")
