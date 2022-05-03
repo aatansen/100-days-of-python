82e5c487feb29176c391c50854154a68f4196d7d
Tansen <aatansen@gmail.com>
2022-05-04 04:34:22 +0600

    Create 129 [Interactive Coding Exercise] Debugging Odd or Even.py

---
+++ 13 Day 13 - Beginner - Debugging_ How to Find and Fix Errors in your Code/129 [Interactive Coding Exercise] Debugging Odd or Even.py
@@ -0,0 +1,6 @@
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0: # here it was asign '=' but it should be '=='
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
