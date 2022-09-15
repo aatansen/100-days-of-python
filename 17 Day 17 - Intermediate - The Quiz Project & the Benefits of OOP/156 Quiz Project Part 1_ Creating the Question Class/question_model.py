acd4c2e23cddb7a022de023a3768da09bd51680e
Tansen <aatansen@gmail.com>
2022-09-16 00:59:12 +0600

    Create question_model.py

---
+++ 17 Day 17 - Intermediate - The Quiz Project & the Benefits of OOP/156 Quiz Project Part 1_ Creating the Question Class/question_model.py
@@ -0,0 +1,8 @@
+class Question:
+    def __init__(self,q_text,answer):
+        self.q_text=q_text
+        self.answer=answer
+
+new_q = Question("sdgegs","False")       
+print(new_q.q_text)
+        
